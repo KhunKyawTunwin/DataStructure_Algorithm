9bcdd3f735bbc9c3e4ee93c245fd846151f64e21
KhunKyaw TunWin <kyawtunwin000782@gmail.com>
2023-12-22 21:15:33 +0630

    Stack and Queue

--- fullcourse/queue.js
+++ fullcourse/queue.js
@@ -16,11 +16,9 @@ queue.add(2);
 queue.add(3);
 queue.add(4);
 queue.add(5);
+queue.add(6);
 console.log("Adding queue data in Array!");
 console.log(queue);
-queue.remove();
-queue.remove();
-queue.remove();
 queue.remove();
 
 console.log("Removing queue data in Array!");
